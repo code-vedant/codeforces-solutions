7de7f982cc3f3431d39b62b2d8881c4be551734f
code-vedant <162135022+code-vedant@users.noreply.github.com>
2025-08-07 21:21:57 +0530

    domino piling A 800

---
+++ dominoPiling.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int m,n;
+    cin >> m >> n;
+    if (m == 0 || n == 0) {
+        cout << "0" << endl;
+        return 0;
+    }
+    int total = (m * n) / 2;
+    cout << total << endl;
+    
+             
+    return 0;
+}

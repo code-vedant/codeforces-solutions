bef2ef6e4191c81578992569713ee82c823a3e42
code-vedant <162135022+code-vedant@users.noreply.github.com>
2025-08-08 20:20:59 +0530

    MAke it Beautiful

---
+++ makeItBeautiful.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int operationOne(vector<int>& a){
+    for(int i = 0 ; i < a.size() ; i++){
+        if(!__builtin)
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n,k;
+        cin >> n >> k;
+
+        vector<int>a(n);
+
+        for(int i = 0 ; i < n ; i++) cin >> a[i];
+
+
+    }
+
+
+    return 0;
+}
